02057ebdb606c3bf41689acff44ff135b4679bc0
Azramulad <106881211+Azramulad@users.noreply.github.com>
2022-11-11 05:30:05 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -22,9 +22,9 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6289526306636'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6289526306636','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
 global.premium = ['6283177360202'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6283177360202' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6289526306636' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
 global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
